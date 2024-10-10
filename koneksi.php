69fc58cf3b2e4394ecfb70de8a979d477ca35273
Deryana <derana765@gmail.com>
2024-10-10 11:11:00 +0700

    Membuat Template SB ADMIN 5 membuat koneksi database dan

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+define("HOST_NAME", "localhost");
+define("USER_NAME", "root");
+define("PASSWORD", "root");
+define("DB_NAME", "app_peminjamanbarang");
+
+$koneksi = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
+?>
