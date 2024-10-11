2d150873a6207b84093448af294bb51426b97ab6
Deryana <derana765@gmail.com>
2024-10-11 23:36:41 +0700

    Membuat Template SB ADMIN 5 membuat koneksi database dan test saja

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-include_once('templates/header.php');
-require_once ('koneksi.php');
+<?php 
+include_once("template/header.php");
+?>
+
+<div class="main-panel m-4">
+    <h1>index</h1>
+</div>
+
+<?php include_once("template/footer.php");
+?>
